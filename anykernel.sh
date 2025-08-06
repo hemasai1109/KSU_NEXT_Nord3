5d66b55520da5a9a4ee7dc496c21e1cd32b93563
HemaSai Nelluri <101991345+hemasai1109@users.noreply.github.com>
2025-08-06 12:53:19 +0530

    Create anykernel.sh

---
+++ anykernel.sh
@@ -0,0 +1,50 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string= DareDevils Nebula kernel with ONEPLUSOSS
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
+
+ui_print "Starting Nika kernel installation..."
+ui_print "Maintainer: DareDevil🪄"
+ui_print "Features:"
+ui_print "-> KernelSU Next "
+ui_print "-> SUSFS ඞ "
+
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/boot" -o -L "/dev/block/by-name/boot" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
